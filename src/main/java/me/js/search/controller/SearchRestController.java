8d76171124b9a15db29675ba28ea0bfae62a5de8
jongseon <shk30290@naver.com>
2021-04-08 09:54:27 +0900

    fix: 불필요한 학습 테스트 및 코드 삭제

--- src/main/java/me/js/search/controller/SearchRestController.java
+++ src/main/java/me/js/search/controller/SearchRestController.java
@@ -6,7 +6,10 @@ import me.js.search.dto.SearchRequest;
 import me.js.search.dto.SearchResponse;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -30,7 +33,8 @@ public class SearchRestController {
         List<SearchResponse> searchResponse = searchService.getSearchResponse(searchRequest);
 
         long duration = (System.nanoTime() - start) / 1_000_000;
-        System.out.println("완료 시간 : " + duration + " msecs");
+
+        log.info("완료 시간 : {} msecs", duration);
 
         return ResponseEntity.ok()
                 .body(searchResponse);
